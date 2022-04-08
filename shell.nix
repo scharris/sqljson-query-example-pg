732427af61fc3300efe51368049f3a3bdc438c67
Stephen Harris <steveofar@hotmail.com>
2022-04-08 17:03:52 -0500

    Remove maven and java.

--- shell.nix
+++ shell.nix
@@ -4,8 +4,6 @@ mkShell {
   buildInputs = [
     nodejs-16_x
     nodePackages.typescript
-    openjdk17
-    maven
   ];
   shellHook = ''
     echo Welcome to the sqljson-query-example-pg project.
